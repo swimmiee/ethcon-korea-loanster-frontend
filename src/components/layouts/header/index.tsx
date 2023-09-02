f788be5bb853ffa583f1528da27f4e5700fa0a49
Sooyoung Lee <dannylisa67@gmail.com>
2023-09-02 21:47:59 +0900

    title

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -3,7 +3,7 @@ import { ConnectWalletButton } from "./connectWallet"
 export const Header = () => {
     return (
         <header className="h-24 px-8 flex items-center justify-between">
-            <p className="text-d-sm">Lending Toaster</p>
+            <p className="text-h-xl">Loanster</p>
             <ConnectWalletButton />
         </header>
     )
