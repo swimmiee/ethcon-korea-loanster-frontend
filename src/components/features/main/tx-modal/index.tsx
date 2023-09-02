fcdda7f359de7e8f0d5e63ddd641df5a7b45b921
Sooyoung Lee <dannylisa67@gmail.com>
2023-09-02 12:27:20 +0900

    transactions draft

--- src/components/features/main/tx-modal/index.tsx
+++ src/components/features/main/tx-modal/index.tsx
@@ -1,12 +1,29 @@
 import Modal from "components/materials/Modal";
+import { useTx } from "./tx.service";
 
 interface TxModalProps {
   close: () => void;
 }
 export const TxModal = ({ close }: TxModalProps) => {
+  const { step, txs, run } = useTx();
   return (
     <Modal title="Lend & Invest" closeModal={close} closable={false}>
-      <div className="py-8"></div>
+      <div className="p-4 flex flex-col">
+        <div className="flex [&>*]:h-6 [&>*]:flex overflow-hidden rounded-lg border-black border-4">
+          <div className="bg-primary" style={{ flex: step + 1 }} />
+          {step < txs.length && <div style={{ flex: txs.length - step }} />}
+        </div>
+
+        <p className="text-center text-t-xl my-4">
+          {step + 1}/{txs.length} {txs[step].description}
+        </p>
+        <button
+          onClick={run}
+          className="rounded-2xl py-3 bg-backgrounds hover:bg-primary border-black border-1.5 "
+        >
+          {txs[step].title}
+        </button>
+      </div>
     </Modal>
   );
 };
--- src/components/features/main/tx-modal/tx.service.ts
+++ src/components/features/main/tx-modal/tx.service.ts
@@ -1,20 +1,93 @@
-import { HEDGE, RANGE, usePosition } from "states/position.state";
+import { Signer } from "ethers";
+import { useState } from "react";
+import { HEDGE, usePosition } from "states/position.state";
 import { ERC20__factory } from "typechain";
 import { useGetSigner } from "utils/useGetSigner";
-import { useAccount } from "wagmi";
 
+interface Tx {
+  title: string;
+  description: string;
+  tx: (signer: Signer) => Promise<void>;
+}
 export const useTx = () => {
-  const { hedge } = usePosition();
-  const {} = useGetSigner()
-  const txs = [];
+  const { hedge, amount, realLong, chain, invest, short } = usePosition();
+  const [step, setStep] = useState<number>(0);
+  const getSigner = useGetSigner();
+  const txs: Tx[] = [];
 
-  // 1. approve to lending pool
+  if (hedge !== HEDGE.NO_HEDGE) {
+    // 1. approve to lending pool
+    const approveTo = "";
+    txs.push({
+      title: "Approve",
+      description: "Approve for lending pool",
+      async tx(signer: Signer) {
+        await ERC20__factory.connect(realLong!.address, signer).approve(
+          approveTo,
+          amount
+        );
+      },
+    });
+
+    // 2. deposit stable coin
+    txs.push({
+      title: "Deposit",
+      description: "Deposit to lending pool",
+      async tx(signer: Signer) {},
+    });
+
+    // 3. borrow short token
+    txs.push({
+      title: "Borrow",
+      description: "Borrow from lending pool",
+      async tx(signer: Signer) {},
+    });
+  }
+
+  // 4. approve long token to toaster contract
+  txs.push({
+    title: "Approve",
+    description: "Approve for toaster contract",
+    async tx(signer: Signer) {
+      await ERC20__factory.connect(realLong!.address, signer).approve(
+        invest!.meta.toaster,
+        amount // TODO
+      );
+    },
+  });
+
+  // 5. approve short token to toaster contract
   if (hedge !== HEDGE.NO_HEDGE) {
     txs.push({
       title: "Approve",
-      description: "Approve to lending pool",
-      status: "pending",
-    //   tx: ERC20__factory.connect(hedge, signer).approve(lendingPool, amount),
+      description: "Approve for toaster contract",
+      async tx(signer: Signer) {
+        await ERC20__factory.connect(short!.address, signer).approve(
+          invest!.meta.toaster,
+          amount // TODO
+        );
+      },
     });
   }
+  // 6. toast
+  txs.push({
+    title: "Provide Liquidity",
+    description: "Provide liquidity to the pool",
+    async tx(signer: Signer) {},
+  });
+
+  const run = async () => {
+    const current = txs[step];
+    const signer = await getSigner(chain.chainId);
+    if (signer) {
+      await current.tx(signer);
+    }
+    setStep((prev) => prev + 1);
+  };
+
+  return {
+    step,
+    txs,
+    run
+  };
 };
